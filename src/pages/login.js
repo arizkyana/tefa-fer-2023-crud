1a11095c6158fb3697701493ae36270da529f20c
agungrizkyana <agung.rizkyana@gmail.com>
2023-07-14 11:21:01 +0700

    chore: init project

---
+++ src/pages/login.js
@@ -0,0 +1,80 @@
+import Layout from '@/components/Layout';
+import { useState } from 'react';
+
+import axios from 'axios';
+import { useRouter } from 'next/router';
+
+export default function Login() {
+  const router = useRouter();
+  const [formValues, setFormValues] = useState({
+    username: null,
+    password: null,
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('https://dummyjson.com/auth/login', {
+        username: formValues.username,
+        password: formValues.password,
+      });
+      if (response.status === 200) {
+        localStorage.setItem('token', response.data.token);
+        router.push('/products');
+      }
+    } catch (error) {
+      console.log('response > ', error);
+    }
+  };
+
+  return (
+    <Layout>
+      <form
+        onSubmit={handleSubmit}
+        className="mx-auto max-w-5xl min-h-screen w-full h-full flex justify-center items-center"
+      >
+        <div className="w-[50%] bg-slate-700 text-white rounded-lg p-6">
+          <h1 className="font-bold text-2xl mb-6 block text-center">Login</h1>
+          <label htmlFor="username" className="block mb-5">
+            <span className="block mb-3">Username</span>
+            <input
+              type="text"
+              className="bg-white p-3 w-full outline-none rounded-lg text-slate-700"
+              name="username"
+              id="username"
+              onChange={(e) =>
+                setFormValues({
+                  username: e.target.value,
+                  password: formValues.password,
+                })
+              }
+              value={formValues?.username}
+            />
+          </label>
+          <label htmlFor="password" className="block mb-5">
+            <span className="block mb-3">Password</span>
+            <input
+              type="password"
+              className="bg-white p-3 w-full outline-none rounded-lg text-slate-700"
+              name="password"
+              id="password"
+              onChange={(e) =>
+                setFormValues({
+                  username: formValues.username,
+                  password: e.target.value,
+                })
+              }
+              value={formValues?.password}
+            />
+          </label>
+          <button
+            type="submit"
+            className="block px-6 py-3 bg-emerald-600 hover:bg-emerald-700 rounded-lg w-full"
+          >
+            Go to my account
+          </button>
+        </div>
+      </form>
+    </Layout>
+  );
+}
---
+++ src/pages/products.js
@@ -0,0 +1,92 @@
+import Layout from '@/components/Layout';
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+
+function ProductItem(props) {
+  return (
+    <div className="bg-slate-700 rounded-lg p-3 min-h-[20rem] relative">
+      <figure className="block h-[10rem] bg-slate-100 rounded-lg ">
+        <img
+          className="w-full h-full object-contain aspect-auto"
+          src={props.thumbnail}
+        />
+      </figure>
+      <div className="py-2">
+        <h3 className="text-lg">{props.title}</h3>
+        <h4 className="text-sm">Rp. {props.price}</h4>
+      </div>
+      <div className="absolute bottom-0 left-0 p-3 w-full">
+        <button
+          type="button"
+          className="border border-emerald-600 bg-white text-emerald-600 block w-full px-5 py-2 hover:bg-emerald-600 hover:text-white rounded-lg text-sm"
+        >
+          add to cart
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default function Products() {
+  const router = useRouter();
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    try {
+      const response = await axios.get(
+        'https://dummyjson.com/auth/products?limit=10',
+        {
+          headers: {
+            Authorization: localStorage.getItem('token')
+              ? `Bearer ${localStorage.getItem('token')}`
+              : null,
+          },
+        }
+      );
+
+      if (response.status === 200) {
+        setProducts(response.data.products);
+      }
+    } catch (error) {
+      console.log('error > ', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.clear();
+    router.replace('/login');
+  };
+
+  return (
+    <Layout>
+      <nav className="bg-slate-700 text-white fixed top-0 left-0 w-full z-50">
+        <div className="mx-auto max-w-6xl py-6 flex justify-between items-center">
+          <h1 className="font-bold text-2xl text-emerald-500">TokoLaku</h1>
+          <div className="flex justify-start items-center space-x-5">
+            <button
+              role="link"
+              type="button"
+              className="block bg-red-500 px-4 py-2 hover:bg-red-600 text-sm rounded-lg"
+              onClick={() => handleLogout()}
+            >
+              Logout
+            </button>
+          </div>
+        </div>
+      </nav>
+      <div className="min-h-screen mx-auto max-w-6xl pt-24">
+        <h1 className="font-bold text-4xl mb-6">Products</h1>
+        <div className="grid grid-cols-4 gap-6">
+          {products &&
+            products.length > 0 &&
+            products.map((item) => <ProductItem {...item} key={item.id} />)}
+        </div>
+      </div>
+    </Layout>
+  );
+}
